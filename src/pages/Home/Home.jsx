bfd50483fccb1dcccc810a499677c6e61267817f
Igor Vinicius <igorviniciusf10@gmail.com>
2022-12-22 11:52:14 -0300

    construction home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import {Container} from "./style";
+import hello from "../../assets/Hello.svg"
 
 export function Home(){
 
+
     return (
         <Container>
-            <h2>Home</h2>
+            <section className="presentation">
+            <img src={hello} alt="hello" className="hello"/>
+
+            <div className="text">
+                <h2 className="tittle">Seja Bem Vindo!!</h2>
+                <p className="line">Nós da My Setup temos a missão de ajudar você a montar seu setup, desde decoração, a melhores periféricos, que vão encaixar no seu estilo e no seu bolso. <br/>
+                Caso tenha dúvida sobre qual produto vale mais apena ou não tem ideia do que comprar entre em contato com nós no <a href="#">Suporte</a>.
+                </p>
+            </div>
+            </section>
+
         </Container>
     )
 }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -2,4 +2,39 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     color: blue;
+    padding-top: 13vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .hello{
+        width: 50rem;
+    }
+
+    .presentation{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .text{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: justify;
+        width: 40%;
+        gap: 10px;
+        font-weight: bolder;
+        color: gray;
+    }
+
+    .tittle{
+        font-size: 2.5rem;
+    }
+
+    .line{
+        font-size: 1.5rem;
+    }
+
 `
